13982df6a4cb80deb4c9af33317093441b6cd195
Alex Alex <alex.nikitin.gm@gmail.com>
2021-09-21 20:06:25 +0300

    ver 1.2

---
+++ TestProj/BrowserTextSelectedEventArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace TestProj
+{
+    public class BrowserTextSelectedEventArgs
+    {
+        public string Text { get; set; }
+
+        public BrowserTextSelectedEventArgs(string text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ TestProj/NewHistoryItemAddedEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TestProj
+{
+    class NewHistoryItemAddedEventArgs:EventArgs
+    {
+        public HistoryDataItem HistoryItem { get; set; }
+        public bool IsCategoryNew { get; set; }
+
+        public NewHistoryItemAddedEventArgs(HistoryDataItem item,bool isCategoryNew)
+        {
+            HistoryItem = item;
+            IsCategoryNew = isCategoryNew;
+        }
+    }
+}
+
